4b759358c909f13116cdc31a3121e759c222f2cb
Joe Lyles <lylesjoe@protonmail.com>
2024-04-29 06:03:46 -0400

    api and middleware

--- server.js
+++ server.js
@@ -1,23 +1,44 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 import 'dotenv/config';
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
+const server = http.createServer(async (req, res) => {
     // res.setHeader('Content-Type', 'text/html');
     // res.statusCode = 404;
     // console.log(req.url);
     // console.log(req.method);
 
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<h1>hello world</h1>');
-    } else if (req.url === '/about') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('<h1>about</h1>');
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<h1>not found</h1>');
+    try {
+        if (req.method === 'GET') {
+            let filePath;
+            if (req.url === '/') {
+                filePath = path.join(__dirname, 'public', 'index.html');
+            } else if (req.url === '/about') {
+                filePath = path.join(__dirname, 'public', 'about.html');
+            } else {
+                throw new Error('not found')
+            }
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+        } else {
+            throw new Error('method not alowed')
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('server error');
     }
+
+
     
     
 });
